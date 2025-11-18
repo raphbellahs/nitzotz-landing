ed36d3f4ad327050be52037a3d9b404975ee2e76
raphael <raphael@elysian-softech.com>
2025-11-19 00:13:21 +0100

    add telephone and email

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,17 +13,18 @@ export const Footer = () => (
   <footer className="mt-12 border-t border-transparent-white py-[5.6rem] text-sm">
     <Container>
       <div className="flex flex-col items-center justify-between lg:flex-row">
-        <div className="flex items-center">
-          <Logo className="mr-4 h-4 w-4" />
-          <span className="text-grey">contact@nitzotz.com</span>
+        <div className="flex flex-col items-center lg:items-start">
+          <Logo className="mb-4 h-4 w-4" />
+          <span className="text-grey">raph.bellahs@gmail.com</span>
+          <span className="text-grey">+33 6 95 07 85 29</span>
         </div>
-        
+
         <div className="my-8 flex space-x-6 text-grey lg:my-0">
           <TwitterIcon />
           <GithubIcon />
           <SlackIcon />
         </div>
-        
+
         <div>
           <Logo className="h-8 w-8" />
         </div>
